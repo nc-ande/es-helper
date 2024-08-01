f5f6f61db69a4cc971e455457d6e1337d38804b5
Andreas Marc Debess <ande@netcompany.com>
2024-08-01 23:11:51 +0200

    Setup map for XML providers.

--- src/logic/exportXmlUtility.ts
+++ src/logic/exportXmlUtility.ts
@@ -1,7 +1,24 @@
 import {CVRRow, PeriodType} from "../model/types";
+import {ParagraphType} from "../config/paragraphs";
+import {producer as sel111} from "./xmlProducers/sel111";
+import {producer as andel} from "./xmlProducers/andel";
+import {producer as fond111} from "./xmlProducers/fond111";
+import {producer as fond112} from "./xmlProducers/fond112";
+import {producer as asc113} from "./xmlProducers/asc113";
+import {producer as asc116} from "./xmlProducers/asc116";
 
 export type ParagraphXmlProducerType = (row: CVRRow, period: PeriodType) => string;
 
-export const exportXml = (paragraph: string, row: CVRRow, period: PeriodType) => {
+const providers: Record<ParagraphType, ParagraphXmlProducerType> = {
+    sel111: sel111,
+    andel: andel,
+    fond111: fond111,
+    fond112: fond112,
+    asc113: asc113,
+    asc116: asc116
+};
+
+export const exportXml = (paragraph: ParagraphType, row: CVRRow, period: PeriodType) => {
+    const xml = providers[paragraph](row, period);
 
 }
