997f56e10d99c3b4cc9bd3836d5b72fa6e6daf65
Michał Klecha <mkle@netcompany.com>
2024-08-01 22:51:34 +0200

    config directory

---
+++ src/config/paragraphs.ts
@@ -0,0 +1,8 @@
+export const Paragraphs = {
+    'sel111': 'Selskab §1.1.1',
+    'andel': 'Andelsforening',
+    'fond111': 'Fond §1.1.1',
+    'fond112': 'Fond §1.1.2',
+    'asc113': 'Forening §1.1.3',
+    'asc116': 'Forening §1.1.6'
+}
